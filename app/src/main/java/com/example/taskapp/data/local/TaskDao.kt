c5c493c34979e56a938eaf6c5bd6817f7d5678a8
GregBiglione <gregory.biglione81@gmail.com>
2025-07-28 04:01:11 +0200

    Added TaskDao

---
+++ app/src/main/java/com/example/taskapp/data/local/TaskDao.kt
@@ -0,0 +1,11 @@
+package com.example.taskapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * from task")
+    fun getAllTasks(): Flow<List<TaskEntity>>
+}
