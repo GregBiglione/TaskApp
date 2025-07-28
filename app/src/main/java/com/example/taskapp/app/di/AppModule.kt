995e6cf4238041c5048d6dd8b325b0d3587b8f07
GregBiglione <gregory.biglione81@gmail.com>
2025-07-28 18:29:29 +0200

    Added AppModule

---
+++ app/src/main/java/com/example/taskapp/app/di/AppModule.kt
@@ -0,0 +1,56 @@
+package com.example.taskapp.app.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.taskapp.data.local.TaskDao
+import com.example.taskapp.data.local.TaskDatabase
+import com.example.taskapp.data.local.TaskRepositoryImplementer
+import com.example.taskapp.domain.repository.TaskRepository
+import com.example.taskapp.domain.usecase.GetAllTasksUseCase
+import com.example.taskapp.domain.usecase.TaskUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    //----------------------------------------------------------------------------------------------
+    // Database
+    //----------------------------------------------------------------------------------------------
+
+    @Provides
+    fun provideTaskDatabase(@ApplicationContext context: Context): TaskDatabase {
+        return Room.databaseBuilder(
+            context,
+            TaskDatabase::class.java,
+            "task_db"
+        ).build()
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // Dao
+    //----------------------------------------------------------------------------------------------
+
+    @Provides
+    fun provideTaskDao(db: TaskDatabase): TaskDao = db.taskDao()
+
+    //----------------------------------------------------------------------------------------------
+    // Repository
+    //----------------------------------------------------------------------------------------------
+
+    @Provides
+    fun provideTaskRepository(dao: TaskDao): TaskRepository = TaskRepositoryImplementer(dao)
+
+    //----------------------------------------------------------------------------------------------
+    // Use case
+    //----------------------------------------------------------------------------------------------
+
+    @Provides
+    fun provideTaskUseCase(taskRepository: TaskRepository): TaskUseCase = TaskUseCase(
+        getAllTasksUseCase = GetAllTasksUseCase(taskRepository)
+    )
+}
