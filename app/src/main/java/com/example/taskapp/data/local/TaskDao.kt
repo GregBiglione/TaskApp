70782a684eb056052bbd4c41fc180f7fbd762e14
GregBiglione <gregory.biglione81@gmail.com>
2025-07-31 21:12:54 +0200

    Added insertTask() in TaskDao

--- app/src/main/java/com/example/taskapp/data/local/TaskDao.kt
+++ app/src/main/java/com/example/taskapp/data/local/TaskDao.kt
@@ -1,11 +1,17 @@
 package com.example.taskapp.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.taskapp.domain.model.Task
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
     @Query("SELECT * from task")
     fun getAllTasks(): Flow<List<TaskEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTask(taskEntity: TaskEntity) {}
 }
