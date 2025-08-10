f84654d4988549bb51b6ade5605ad70f5b881a80
GregBiglione <gregory.biglione81@gmail.com>
2025-08-11 01:21:54 +0200

    Added RemoveTaskUseCase & added it in TaskUseCase

--- app/src/main/java/com/example/taskapp/domain/usecase/TaskUseCase.kt
+++ app/src/main/java/com/example/taskapp/domain/usecase/TaskUseCase.kt
@@ -3,5 +3,6 @@ package com.example.taskapp.domain.usecase
 data class TaskUseCase(
     val getAllTasksUseCase: GetAllTasksUseCase,
     val insertTaskUseCase: InsertTaskUseCase,
-    val updateTaskUseCase: UpdateTaskUseCase
+    val updateTaskUseCase: UpdateTaskUseCase,
+    val removeTaskUseCase: RemoveTaskUseCase,
 )
