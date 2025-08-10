8307533fbf731e926c3dbc0668e38a42b29f9f6a
GregBiglione <gregory.biglione81@gmail.com>
2025-08-11 01:00:54 +0200

    Added removeTask() in TaskDao

--- app/src/main/java/com/example/taskapp/data/local/TaskDao.kt
+++ app/src/main/java/com/example/taskapp/data/local/TaskDao.kt
@@ -1,6 +1,7 @@
 package com.example.taskapp.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -17,4 +18,7 @@ interface TaskDao {
 
     @Update
     suspend fun updateTask(taskEntity: TaskEntity)
+
+    @Delete
+    suspend fun removeTask(taskEntity: TaskEntity)
 }
